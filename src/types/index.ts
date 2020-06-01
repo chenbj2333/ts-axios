88dde05a6d5030203fa459000cbb98a42b4dd9f1
cocobean <1749402844@qq.com>
2020-06-01 22:58:06 +0800

    核心功能

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export type Method =
+  | 'GET'
+  | 'get'
+  | 'post'
+  | 'POST'
+  | 'delete'
+  | 'DELETE'
+  | 'put'
+  | 'PUT'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {}
